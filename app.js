a05df3848176908e9d9b47c3130d83923c8a47a5
Kateryna Karpova <katyakarpova@gmail.com>
2021-12-30 10:57:36 +0100

    Update app.js
    deleted old comment

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/users', usersRouter)
-// app.use('/api/user', usersRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
